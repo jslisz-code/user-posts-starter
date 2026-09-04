805107cefc435904b6f6be5eb3df027f8202e7df
jslisz-code <jslisz.code@outlook.com>
2026-09-04 01:16:12 -0500

    debugging

--- user.js
+++ user.js
@@ -1,4 +1,9 @@
 const postListEl = document.querySelector(".post-list");
+
+function onSearchChange() {
+  console.log('onSearchChange');
+}
+
 async function main() {
   const id = localStorage.getItem("id");
   const posts = await fetch(
