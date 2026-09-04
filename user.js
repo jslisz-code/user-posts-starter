92973cb39ebaa056bbc5c3f0dd0cb17d426f0ffc
jslisz-code <jslisz.code@outlook.com>
2026-09-04 03:58:58 -0500

    debugging unpopulated user tags

--- user.js
+++ user.js
@@ -4,39 +4,25 @@ function onSearchChange(event) {
   const id = event.target.value;
   renderPosts(id);
 }
-const posts = await fetch(
-  `https://jsonplaceholder.typicode.com/posts?userId=${id}`,
-);
-const postsData = await posts.json();
-postListEl.innerHTML = postsData
-  .map(
-    (post) => `
-      <div class="post">
-        <div class="post__title">${post.title}</div>
-        <p class="post__body">${post.body}</p>
-      </div>
-  `,
-  )
-  .join("");
 
-async function main() {
-  const id = localStorage.getItem("id");
+async function renderPosts(id) {
   const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`,
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
+
   const postsData = await posts.json();
-  console.log(postsData);
 
   postListEl.innerHTML = postsData
     .map(
       (post) => `
-      <div class="post">
-        <div class="post__title">${post.title}</div>
-        <p class="post__body">${post.body}</p>
-      </div>
-  `,
+        <div class="post">
+          <div class="post__title">${post.title}</div>
+          <p class="post__body">${post.body}</p>
+        </div>
+      `
     )
     .join("");
 }
+
 const id = localStorage.getItem("id");
 renderPosts(id);
