ff31db949425153e379c6d1b17528338a6f2942d
jslisz-code <jslisz.code@outlook.com>
2026-09-04 02:05:43 -0500

    debugging content population

--- user.js
+++ user.js
@@ -1,16 +1,23 @@
 const postListEl = document.querySelector(".post-list");
 
-function onSearchChange() {
-  console.log('onSearchChange');
+function onSearchChange(event) {
+  const id = event.target.value;
+  renderPosts(id);
 }
 
-async function main() {
-  const id = localStorage.getItem("id");
+postListEl.innerHTML = postsData.map(post => `
+      <div class="post">
+        <div class="post__title">${post.title}</div>
+        <p class="post__body">${post.body}</p>
+      </div>
+  `).join("");
+
+async function renderPosts(id) {
+  
   const posts = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${id}`,
-  );
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`,);
   const postsData = await posts.json();
-  console.log(postsData);
+  console.log(postsData)
 
   postListEl.innerHTML = postsData
     .map(
@@ -23,5 +30,5 @@ async function main() {
     )
     .join("");
 }
-
-main();
+const id = localStorage.getItem("id");
+renderPosts();
